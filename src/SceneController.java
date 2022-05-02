69be6162498095e36fe3456b080df0ad81244b38
Pramesh Shrestha <prsh37959@edu.ucl.dk>
2022-05-02 14:15:34 +0200

    hjk

--- src/SceneController.java
+++ src/SceneController.java
@@ -2,9 +2,12 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,14 +17,16 @@ public class SceneController
   private Stage stage;
   private Scene scene;
   private FXMLLoader loader;
+
+  @FXML private AnchorPane changeAnchorPane;
   @FXML private MenuItem createBooking;
   @FXML private Button backToMainPage;
+  @FXML private Button check;
 
 
   //creating methods to switch between scenes
   public void switchToMainPageScene(ActionEvent event) throws IOException
   {
-
     loader = new FXMLLoader();
     loader.setLocation(getClass().getResource("mainPage.fxml"));
     stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
@@ -38,10 +43,24 @@ public class SceneController
     scene = new Scene(loader.load());
     stage.setScene(scene);
     stage.show();
-  }
 
-  public void lastName(ActionEvent event)
-  {
-
+//    changeAnchorPane.getChildren().clear();
+//    changeAnchorPane.getChildren().setAll(loadFXML("createBookingScene.fxml"));
   }
+//  private Parent loadFXML(String path){
+//    javafx.fxml.FXMLLoader loader = new javafx.fxml.FXMLLoader();
+//    loader.setLocation(getClass().getResource(path));
+//    Parent root = null;
+//    try
+//    {
+//      root = loader.load();
+//    }
+//    catch (IOException e)
+//    {
+//      e.printStackTrace();
+//    }
+//    return root;
+//  }
+
+
 }
